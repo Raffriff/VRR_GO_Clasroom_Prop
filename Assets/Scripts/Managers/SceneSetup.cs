163442a5c057e74448b5da800d20fc5614a2e371
benguiden <benguiden@hotmail.co.uk>
2018-04-09 17:30:12 +0100

    Added lobby functionality.

---
+++ Assets/Scripts/Managers/SceneSetup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSetup : MonoBehaviour {
+
+    #region Public Variables
+    public static SceneSetup main;
+
+    [Header ("References")]
+    public GameObject[] localTraineeActivate;
+    public GameObject[] localTraineeDeactivate, localCompanionActivate, localCompaniondeactivate;
+    #endregion
+
+    #region Mono Methods
+    private void Awake() {
+        main = this;
+        StartSettingUp ();
+    }
+    #endregion
+
+    #region Public Methods
+    public void StartSettingUp() {
+        if (MultiplayerManager.main.playerType == MultiplayerManager.LocalPlayerType.Trainee) {
+            foreach (GameObject toActivate in localTraineeActivate) {
+                toActivate.SetActive (true);
+            }
+            foreach (GameObject toDeactivate in localTraineeDeactivate) {
+                toDeactivate.SetActive (false);
+            }
+        } else {
+            foreach (GameObject toActivate in localCompanionActivate) {
+                toActivate.SetActive (true);
+            }
+            foreach (GameObject toDeactivate in localCompaniondeactivate) {
+                toDeactivate.SetActive (false);
+            }
+        }
+    }
+    #endregion
+
+    
+
+}
