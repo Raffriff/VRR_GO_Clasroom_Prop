01d7741deeb623906565fe5db839e0ec36fa999b
benguiden <benguiden@hotmail.co.uk>
2018-04-17 16:11:46 +0100

    Improved physics networking.

---
+++ Assets/Scripts/Networking/PhysicsNetworker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsNetworker : MonoBehaviour {
+
+    #region Public Variables
+    public List<Rigidbody> rigidbodies;
+
+    public PhotonView photonView;
+    #endregion
+
+    #region Public Methods
+    public void AddRigidbody(Rigidbody newRigidbody) {
+        rigidbodies.Add (newRigidbody);
+    }
+    #endregion
+
+    #region Network Methods
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+
+    }
+    #endregion
+
+}
